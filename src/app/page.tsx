03c7a22bdc8931c5322086a88ed313e57efcdbcb
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2023-08-15 11:14:50 -0300

    :bookmark: rotomdex.io 1.3.0 - :construction: Tentando uma nova forma de fazer as requisições, de forma gradual (de 20 em 20)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,67 +4,66 @@ import { useEffect, useMemo, useState } from 'react'
 import { Loading } from '@/components/Loading'
 import { PokemonCard } from '@/components/PokemonCard'
 
-import Pokedex from 'pokedex-promise-v2'
+import Pokedex, { Pokemon } from 'pokedex-promise-v2'
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [loadingMessage, setLoadingMessage] = useState('Carregando API')
+  // const [isLoading, setIsLoading] = useState(true)
+  // const [loadingMessage, setLoadingMessage] = useState('Carregando API')
 
-  const pokemonNames: string[] = useMemo(() => [], [])
-  const pokemons: Pokedex.Pokemon[] = useMemo(() => [], [])
+  // const pokemonNames: string[] = useMemo(() => [], [])
+  // const pokemons: Pokedex.Pokemon[] = useMemo(() => [], [])
 
-  useEffect(() => {
-    ;(async function getAllPokemons() {
-      const pokedex = new Pokedex()
+  // useEffect(() => {
+  //   ;(async function getAllPokemons() {
+  //     const pokedex = new Pokedex()
 
-      // Primeiro, obtemos os nomes de todos os Pokemons
-      await pokedex
-        .getPokemonsList({ offset: 0, limit: 1281 })
-        .then((response) =>
-          response.results.forEach((pokemonResponse) =>
-            pokemonNames.push(pokemonResponse.name),
-          ),
-        )
-        .finally(() => setLoadingMessage('Carregando Pokemons'))
+  //     // Primeiro, obtemos os nomes de todos os Pokemons
+  //     await pokedex
+  //       .getPokemonsList({ offset: 0, limit: 1281 })
+  //       .then((response) =>
+  //         response.results.forEach((pokemonResponse) =>
+  //           pokemonNames.push(pokemonResponse.name),
+  //         ),
+  //       )
+  //       .finally(() => setLoadingMessage('Carregando Pokemons'))
 
-      // Logo após, realizamos uma requisição na API para cada pokemon
-      await pokedex
-        .getPokemonByName(pokemonNames)
-        .then((response) =>
-          response.forEach((pokemon) => pokemons.push(pokemon)),
-        )
-        .finally(() => setIsLoading(false))
-    })()
-  }, [pokemonNames, pokemons])
+  //     // Logo após, realizamos uma requisição na API para cada pokemon
+  //     await pokedex
+  //       .getPokemonByName(pokemonNames)
+  //       .then((response) =>
+  //         response.forEach((pokemon) => pokemons.push(pokemon)),
+  //       )
+  //       .finally(() => setIsLoading(false))
+  //   })()
+  // }, [pokemonNames, pokemons])
+  const [pokemonList, setPokemonsList] = useState<Pokemon[]>([])
+  const [offset, setOffset] = useState(0)
+  const [limit, setLimit] = useState(20)
+  const pokemonNames: string[] = []
+
+  const pokedex = new Pokedex({ timeout: 10 * 5000, cacheLimit: 100 * 5000 })
+
+  async function getPokemonNamesList(offset: number, limit: number) {
+    const response = await pokedex.getPokemonsList({ offset, limit })
+
+    response.results.forEach((result) => {
+      pokemonNames.push(result.name)
+    })
+  }
+
+  getPokemonNamesList(offset, limit)
+
+  function updateOffsetLimit() {
+    setOffset(offset + limit)
+    setLimit(limit + 20)
+
+    getPokemonNamesList(offset, limit)
+  }
 
   return (
     <main>
-      {isLoading && <Loading loadingMessage={loadingMessage} />}
-      <div className="grid grid-cols-4">
-        {pokemons.map((pokemon) => (
-          <PokemonCard
-            key={pokemon.id}
-            id={pokemon.id}
-            name={pokemon.name}
-            base_experience={pokemon.base_experience}
-            height={pokemon.height}
-            is_default={pokemon.is_default}
-            order={pokemon.order}
-            weight={pokemon.weight}
-            abilities={pokemon.abilities}
-            forms={pokemon.forms}
-            game_indices={pokemon.game_indices}
-            held_items={pokemon.held_items}
-            location_area_encounters={pokemon.location_area_encounters}
-            moves={pokemon.moves}
-            sprites={pokemon.sprites}
-            species={pokemon.species}
-            stats={pokemon.stats}
-            types={pokemon.types}
-            past_types={pokemon.past_types}
-          />
-        ))}
-      </div>
+      <div className="grid grid-cols-4 gap-6"></div>
+      <button onClick={updateOffsetLimit}>Carregar mais</button>
     </main>
   )
 }
